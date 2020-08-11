ff8f138709fd61416db968a3cf6122063c044504
Mike Solomon <mike@meeshkan.com>
2020-08-11 11:26:39 +0300

    Adds stuff

--- src/Main.purs
+++ src/Main.purs
@@ -1,13 +1,24 @@
 module Main where
 
 import Prelude
-import Data.Array (many, some)
-import Data.String.CodeUnits (fromCharArray)
+import Control.Alt ((<|>))
+import Control.Lazy (fix)
+import Data.Array (fold, many, some)
+import Data.Char.Unicode (isAlphaNum)
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
+import Data.List (List)
+import Data.String.CodeUnits (fromCharArray, singleton)
+import Data.Traversable (sequence)
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Console (log)
-import Text.Parsing.Parser (Parser)
-import Text.Parsing.Parser.String (class StringLike, oneOf)
+import Global (readFloat)
+import Text.Parsing.Parser (Parser, fail)
+import Text.Parsing.Parser.Combinators (between, option, sepBy, try)
+import Text.Parsing.Parser.String (class StringLike, char, eof, oneOf, satisfy, string, whiteSpace)
 
+-- use eof for the end of a file when parsing files
 parse_ba :: forall s. StringLike s => Parser s Char
 parse_ba = oneOf [ 'a', 'b' ]
 
@@ -20,6 +31,130 @@ sanityCheckSome = fromCharArray <$> some_ba
 sanityCheckMany :: forall s. StringLike s => Parser s String
 sanityCheckMany = fromCharArray <$> (many $ parse_ba)
 
-main :: Effect Unit
-main = do
-  log "🍝"
+parseNull :: forall s. StringLike s => Parser s Json
+parseNull = string "null" *> pure JNull
+
+parseBoolean :: forall s. StringLike s => Parser s Json
+parseBoolean =
+  (string "true" *> pure (JBoolean true))
+    <|> (string "false" *> pure (JBoolean false))
+
+negativeSign :: forall s. StringLike s => Parser s String
+negativeSign = string "-"
+
+integerWhen0 :: forall s. StringLike s => Parser s String
+integerWhen0 = append <$> (option "" negativeSign) <*> (string "0")
+
+oneToNine = [ '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
+
+digits = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
+
+integerWhenNot0 :: forall s. StringLike s => Parser s String
+integerWhenNot0 =
+  fold
+    <$> sequence
+        [ (option "" negativeSign)
+        , (singleton <$> oneOf oneToNine)
+        , (fromCharArray <$> (many $ oneOf digits))
+        ]
+
+integerPart :: forall s. StringLike s => Parser s String
+integerPart = (try integerWhen0) <|> integerWhenNot0
+
+fractionalPart :: forall s. StringLike s => Parser s String
+fractionalPart = append <$> (string ".") <*> (fromCharArray <$> (many $ oneOf digits))
+
+floatWithFraction :: forall s. StringLike s => Parser s String
+floatWithFraction = append <$> integerPart <*> fractionalPart
+
+exponentPart :: forall s. StringLike s => Parser s String
+exponentPart =
+  fold
+    <$> sequence
+        [ (singleton <$> oneOf [ 'e', 'E' ])
+        , (option "" (singleton <$> oneOf [ '+', '-' ]))
+        , (fromCharArray <$> (many $ oneOf digits))
+        ]
+
+floatWithExponent :: forall s. StringLike s => Parser s String
+floatWithExponent = append <$> integerPart <*> exponentPart
+
+floatWithDigitAndExponent :: forall s. StringLike s => Parser s String
+floatWithDigitAndExponent =
+  fold
+    <$> sequence
+        [ integerPart
+        , fractionalPart
+        , exponentPart
+        ]
+
+parseNumber :: forall s. StringLike s => Parser s Json
+parseNumber =
+  (JNumber <<< readFloat)
+    <$> ( (try floatWithFraction)
+          <|> (try floatWithExponent)
+          <|> (try floatWithDigitAndExponent)
+          <|> integerPart
+      )
+
+parseString' :: forall s. StringLike s => Parser s String
+parseString' =
+  between
+    (string "\"")
+    (string "\"")
+    (fromCharArray <$> many (satisfy isAlphaNum))
+
+parseString :: forall s. StringLike s => Parser s Json
+parseString = JString <$> parseString'
+
+parseArray :: forall s. StringLike s => Parser s Json -> Parser s Json
+parseArray pj =
+  JArray
+    <$> ( between
+          (string "[" *> whiteSpace)
+          (whiteSpace *> string "]")
+          (sepBy (whiteSpace *> pj <* whiteSpace) (char ','))
+      )
+
+parseKeyValuePair :: forall s. StringLike s => Parser s Json -> Parser s (Tuple String Json)
+parseKeyValuePair pj =
+  Tuple
+    <$> parseString'
+    <*> (whiteSpace *> char ':' *> whiteSpace *> pj)
+
+parseObject :: forall s. StringLike s => Parser s Json -> Parser s Json
+parseObject pj =
+  JObject
+    <$> ( between
+          (string "{" *> whiteSpace)
+          (whiteSpace *> string "}")
+          (sepBy (whiteSpace *> parseKeyValuePair pj <* whiteSpace) (char ','))
+      )
+
+parseJson :: forall s. StringLike s => Parser s Json
+parseJson =
+  fix \me ->
+    (try (parseObject me))
+      <|> (try (parseArray me))
+      <|> (try parseNumber)
+      <|> (try parseString)
+      <|> (try parseBoolean)
+      <|> (parseNull)
+
+-- -3.0E-7
+-- 4
+-- 1.2
+data Json
+  = JNumber Number -- 1.0
+  | JString String -- "hello world"
+  | JNull -- null
+  | JBoolean Boolean -- true
+  | JArray (List Json) -- [ 1 , 2 ]
+  | JObject (List (Tuple String Json)) -- {"a":"b","a":"c"}
+
+derive instance genericJson :: Generic Json _
+
+derive instance eqJson :: Eq Json
+
+instance jsonShow :: Show Json where
+  show a = genericShow a
--- test/Main.purs
+++ test/Main.purs
@@ -2,9 +2,11 @@ module Test.Main where
 
 import Prelude
 import Data.Either (Either(..), isLeft)
+import Data.List (List(..))
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Main (sanityCheckMany, sanityCheckSome)
+import Main (Json(..), floatWithDigitAndExponent, floatWithExponent, floatWithFraction, integerPart, integerWhen0, integerWhenNot0, parseBoolean, parseJson, parseNull, parseNumber, parseString, sanityCheckMany, sanityCheckSome)
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter (consoleReporter)
@@ -22,3 +24,67 @@ main = do
             true `shouldEqual` isLeft (runParser "caaaaaaaaaaaa" sanityCheckSome)
             Right "" `shouldEqual` (runParser "caaaaaaaaaaaa" sanityCheckMany)
             Right "abba" `shouldEqual` (runParser "abba" sanityCheckSome)
+            Right "aaaaa" `shouldEqual` (runParser "aaaaaca" sanityCheckSome)
+        describe "json primitives" do
+          it "should parse an 0 when integer" do
+            Right "0" `shouldEqual` (runParser "0" integerWhen0)
+            Right "-0" `shouldEqual` (runParser "-0" integerWhen0)
+            true `shouldEqual` isLeft (runParser "1" integerWhen0)
+          it "should parse a non-zero integer" do
+            Right "42" `shouldEqual` (runParser "42" integerWhenNot0)
+            Right "1" `shouldEqual` (runParser "1" integerWhenNot0)
+            Right "-1" `shouldEqual` (runParser "-1" integerWhenNot0)
+            true `shouldEqual` isLeft (runParser "0" integerWhenNot0)
+          it "should parse an integer part" do
+            Right "42" `shouldEqual` (runParser "42" integerPart)
+            Right "0" `shouldEqual` (runParser "0" integerPart)
+            Right "-42" `shouldEqual` (runParser "-42" integerPart)
+          it "should parse a simple float" do
+            Right "3.1416" `shouldEqual` (runParser "3.1416" floatWithFraction)
+            Right "-3.1416" `shouldEqual` (runParser "-3.1416" floatWithFraction)
+          it "should parse an exponent float" do
+            Right "3e10" `shouldEqual` (runParser "3e10" floatWithExponent)
+            Right "-3E-4" `shouldEqual` (runParser "-3E-4" floatWithExponent)
+            Right "-3E+4" `shouldEqual` (runParser "-3E+4" floatWithExponent)
+          it "should parse an exponent float with digits" do
+            Right "3.1e10" `shouldEqual` (runParser "3.1e10" floatWithDigitAndExponent)
+            Right "-3.1E-4" `shouldEqual` (runParser "-3.1E-4" floatWithDigitAndExponent)
+            Right "-3.1E+4" `shouldEqual` (runParser "-3.1E+4" floatWithDigitAndExponent)
+        describe "json parser" do
+          it "should parse null" do
+            Right JNull `shouldEqual` (runParser "null" parseNull)
+            true `shouldEqual` isLeft (runParser "foo" parseNull)
+          it "should parse boolean" do
+            Right (JBoolean true) `shouldEqual` (runParser "true" parseBoolean)
+            Right (JBoolean false) `shouldEqual` (runParser "false" parseBoolean)
+            true `shouldEqual` isLeft (runParser "foo" parseBoolean)
+          it "should parse a (admittedly simple) string" do
+            Right (JString "hello") `shouldEqual` (runParser "\"hello\"" parseString)
+            true `shouldEqual` isLeft (runParser "foo" parseString)
+          it "should parse number" do
+            Right (JNumber 3.1416) `shouldEqual` (runParser "3.1416" parseNumber)
+            Right (JNumber 3.0) `shouldEqual` (runParser "3" parseNumber)
+            Right (JNumber (negate 3.1416)) `shouldEqual` (runParser "-3.1416" parseNumber)
+          it "should parse an array" do
+            Right (JArray Nil) `shouldEqual` (runParser "[]" parseJson)
+            Right (JArray (Cons (JNumber 1.0) Nil)) `shouldEqual` (runParser "[1]" parseJson)
+            Right (JArray (Cons (JNumber 1.0) Nil)) `shouldEqual` (runParser "[1 ]" parseJson)
+            Right (JArray (Cons (JNumber 1.0) Nil)) `shouldEqual` (runParser "[ 1]" parseJson)
+            Right (JArray (Cons (JNumber 1.0) Nil)) `shouldEqual` (runParser "[ 1 ]" parseJson)
+            true `shouldEqual` isLeft (runParser "[foo]" parseJson)
+          it "should parse an object" do
+            Right (JObject Nil) `shouldEqual` (runParser "{}" parseJson)
+            Right (JObject (Cons (Tuple "a" (JNumber 1.0)) Nil)) `shouldEqual` (runParser "{\"a\":1}" parseJson)
+            Right (JObject (Cons (Tuple "a" (JNumber 1.0)) Nil)) `shouldEqual` (runParser "{ \"a\":1}" parseJson)
+            Right (JObject (Cons (Tuple "a" (JNumber 1.0)) Nil)) `shouldEqual` (runParser "{ \"a\" :1}" parseJson)
+            Right (JObject (Cons (Tuple "a" (JNumber 1.0)) Nil)) `shouldEqual` (runParser "{ \"a\" :   1}" parseJson)
+            Right (JObject (Cons (Tuple "a" (JNumber 1.0)) Nil)) `shouldEqual` (runParser "{ \"a\" :   1    }" parseJson)
+            Right
+              ( JObject
+                  ( Cons (Tuple "a" (JNumber 1.0))
+                      (Cons (Tuple "b" (JNumber 2.0)) Nil)
+                  )
+              )
+              `shouldEqual`
+                (runParser "{ \"a\" :   1  , \"b\": 2.0 }" parseJson)
+            true `shouldEqual` isLeft (runParser "[foo]" parseJson)
